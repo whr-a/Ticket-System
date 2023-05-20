64d624d0c5b41a6881dacdca2bb44d663ee788bc
whr <2472356642@qq.com>
2023-05-21 02:55:13 +0800

    null

--- database_copy.hpp
+++ database_copy.hpp
@@ -42,7 +42,7 @@ private:
             return !(obj2<obj1);
         }
     };
-    static const int size_of_block=100;
+    static const int size_of_block=40;
     std::fstream opfile;
 
     class start
